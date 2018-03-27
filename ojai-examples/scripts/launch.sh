4d098fc0bb35d326a1fa7e922122f0ca137891b5
xuelan <xuelan.sun@gmail.com>
2018-03-28 01:14:59 +0200

    Add links

--- ojai-examples/scripts/launch.sh
+++ ojai-examples/scripts/launch.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Useful links :
+# https://community.mapr.com/thread/22280-maprdb-jsoncan-an-application-connect-without-mapr-client
+# https://community.mapr.com/docs/DOC-1420
+
 export MAPR_TICKETFILE_LOCATION=/tmp/maprticket_5000
 
 java -Dhadoop.login=mapr -Dhadoop.login=hybrid -Dzookeeper.saslprovider=com.mapr.security.maprsasl.MaprSaslProvider -Dmapr.home.dir=/tmp/ -Djava.security.auth.login.config=/tmp/conf/mapr.login.conf -cp "/Users/xuelansun/Git/mapr-db-60-getting-started/ojai-examples/target/*"  com.mapr.db.examples.OJAI_001_YelpSimpleQuery
